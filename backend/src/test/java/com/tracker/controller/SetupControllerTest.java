0a78f66a59af043569210a393164dc6b5aa52215
Stefan Zaufl <stefan.zaufl@,gmail.com>
2026-04-08 11:32:32 +0000

    Stabilize setup controller test cleanup

--- backend/src/test/java/com/tracker/controller/SetupControllerTest.java
+++ backend/src/test/java/com/tracker/controller/SetupControllerTest.java
@@ -1,5 +1,14 @@
 package com.tracker.controller;
 
+import com.tracker.model.entity.SetupState;
+import com.tracker.repository.CategoryRepository;
+import com.tracker.repository.FileUploadRepository;
+import com.tracker.repository.PaymentPeriodHistoryRepository;
+import com.tracker.repository.RecurringPaymentRepository;
+import com.tracker.repository.RuleRepository;
+import com.tracker.repository.SetupStateRepository;
+import com.tracker.repository.TransactionRecurringLinkRepository;
+import com.tracker.repository.TransactionRepository;
 import com.tracker.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,9 +37,42 @@ class SetupControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PaymentPeriodHistoryRepository historyRepository;
+
+    @Autowired
+    private TransactionRecurringLinkRepository linkRepository;
+
+    @Autowired
+    private RuleRepository ruleRepository;
+
+    @Autowired
+    private RecurringPaymentRepository recurringPaymentRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private FileUploadRepository fileUploadRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private SetupStateRepository setupStateRepository;
+
     @BeforeEach
     void setUp() {
+        historyRepository.deleteAll();
+        linkRepository.deleteAll();
+        ruleRepository.deleteAll();
+        recurringPaymentRepository.deleteAll();
+        transactionRepository.deleteAll();
+        fileUploadRepository.deleteAll();
+        categoryRepository.deleteAll();
         userRepository.deleteAll();
+        setupStateRepository.deleteAll();
+        setupStateRepository.save(new SetupState());
     }
 
     @Test
